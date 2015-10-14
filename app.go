4bb8fe9963ddc60ae934b75590a5fc48641ffe30
charlie austin <charlie.austin@intentmedia.com>
2015-10-14 16:30:41 -0400

    Adding in some routing.

--- app.go
+++ app.go
@@ -1,21 +1,36 @@
 package main
 
 import (
-        "log"
-        "net/http"
-       )
+	"io"
+	"log"
+	"net/http"
+	"regexp"
+)
 
 func main() {
 
-    http.HandleFunc("/", homeHandler)
-    http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
-                http.ServeFile(w, r, r.URL.Path[1:])
-    })
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/data/[[:alpha:]]", dataHandler)
+	mux.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
 
-    log.Println("Listening...")
-    http.ListenAndServe(":3000", nil)
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", mux)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "static/main.html")
+	log.Println("home handler serving request")
+	log.Println(r)
+	http.ServeFile(w, r, "static/main.html")
+}
+
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("data handler serving request")
+	log.Println(r)
+	re := regexp.MustCompile(".*/[[:alpha:]]$")
+	path := r.URL.Path
+	food := re.FindString(path)
+	io.WriteString(w, "Hello "+food)
 }
