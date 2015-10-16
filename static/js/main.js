bdbe551252dbcbf91ec0c9dc21232b42d083fca9
Charlton Austin <charlton.austin@gmail.com>
2015-10-15 20:59:56 -0400

    Adding in something to show the text and to unfocus.

--- static/js/main.js
+++ static/js/main.js
@@ -3,12 +3,16 @@ var viewModel = {
     name: ko.observable(),
     gallonPerCalorie: ko.observable(),
     searchFood: function(formElement) {
+        var tmp = document.createElement("input");
+        document.body.appendChild(tmp);
+        tmp.focus();
+        document.body.removeChild(tmp);
         $.get( "http://localhost:3000/data/" +formElement.food_input.value, function( data ) {
             var myData = JSON.parse(data)
             viewModel.name(myData.Name)
             viewModel.gallonPerCalorie(myData.GallonPerCalorie)
             viewModel.shouldShowDetails(true)
-            
+
         });
     },
 };
